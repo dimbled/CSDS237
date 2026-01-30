aa1fee93b8c15966b05c4307f26b3bdccf053e7d
Aaron <aaron.erlanger@gmail.com>
2026-01-30 14:44:34 -0500

    did WordFinder.py

---
+++ WordFinder.py
@@ -0,0 +1,15 @@
+﻿# IMPORTS
+
+_sent = input("Enter sentence: ") # get input
+_splitSent = _sent.split() # split the input into words
+_fixedSplitSent = _splitSent.copy() # create a copy to fix up
+_longestLen = 0 # initialize longest word length at 0
+_longestID = 0 # initialize id of longest word at 0
+
+for i in range(len(_fixedSplitSent)): # iterate through each word in the array of words
+    _fixedSplitSent[i] = "".join(filter(str.isalpha, _fixedSplitSent[i])) # fix each word by removing nonalpha chars
+    if (len(_fixedSplitSent[i]) > _longestLen): # if the current word is the longest
+        _longestLen = len(_fixedSplitSent[i]) # update the longest word length
+        _longestID = i # update the position of the longest word for retrieval
+
+print("The longest word in the sentence: " + _sent + " is " + _splitSent[_longestID] + " with length of " + str(_longestLen) + " characters.")
