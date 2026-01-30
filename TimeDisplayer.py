74e4dcb49b72eaddd21717b3e11e2ff83a09058d
Aaron <aaron.erlanger@gmail.com>
2026-01-30 15:00:13 -0500

    did TimeDisplayer.py

---
+++ TimeDisplayer.py
@@ -0,0 +1,8 @@
+﻿#IMPORTS
+import datetime
+
+# Display the current date and time in 'US/Pacific' time zone.
+_tz = datetime.timezone(datetime.timedelta(hours=-8)) # create the time zone relative to UTC (DST is in effect)
+_dt = datetime.datetime.now(_tz) # get the time
+
+print(_dt) # display the time
