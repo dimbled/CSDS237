d934712b84f1f04f6681731c9f38bb05afae1554
Aaron <aaron.erlanger@gmail.com>
2026-01-30 13:43:29 -0500

    did PrimeChecker.py

---
+++ PrimeChecker.py
@@ -0,0 +1,18 @@
+﻿import math
+
+_numberToCheck = input("Enter a number, and I'll check if it is prime: ")
+_n = int(_numberToCheck)
+_sqrt = int(math.sqrt(_n) + 1)
+isPrime = True # default to true
+
+if (_n <= 1): #check for 1 and negative numbers
+    isPrime = False # 1 is not prime, but passes the standard logic
+else: # most other inputs will go here
+    for i in range(2, _sqrt): # standard logic
+        if ((_n / i) % 1 == 0): # if a factor is found
+            isPrime = False # update prime status
+            break # and print
+
+print(_numberToCheck + " is a prime number.") if isPrime else print(_numberToCheck + " is not a prime number.")
+
+
