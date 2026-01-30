ada5f7cf778b115a67192c66d08f869c94b56885
Aaron <aaron.erlanger@gmail.com>
2026-01-30 12:47:41 -0500

    initial commit with first problem done

---
+++ pythonDataTypes.py
@@ -0,0 +1,8 @@
+﻿# IMPORTS
+import math
+
+radius = input("Enter radius: ")
+_r = float(radius)
+volume = math.pi * _r ** 3
+print("The volume of a sphere with radius" + radius + " is " + str(volume))
+
