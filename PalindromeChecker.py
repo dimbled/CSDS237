7f72254ec0d60e37e31a65a8d1aaae22b7cc9113
Aaron <aaron.erlanger@gmail.com>
2026-01-30 14:16:35 -0500

    did PalindromeChecker.py, need to ask about StringReader.py

---
+++ PalindromeChecker.py
@@ -0,0 +1,14 @@
+﻿#IMPORTS
+
+_str = input("Enter a string, and I will check if it is a palindrome: ")
+_fixedStr = "".join(filter(str.isalnum, _str)).lower() # eliminate all nonalphanumeric characters and convert to lowercase
+_len = len(_fixedStr) # get the length of the formatted string
+_halfLen = int(len(_fixedStr) / 2) # this is how many times we will check (only need to check half of the string)
+isPalindrome = True # default to true
+
+for i in range(_halfLen): # iterate through half the string from either direction
+    if _fixedStr[i] != _fixedStr[_len - i - 1]: # check if the character at the index and "reverse index" are the same
+        isPalindrome = False # if they don't match, it's not a palindrome
+        break # break out of logic if a non-matching pair is found
+
+print(_str + " is a palindrome.") if isPalindrome else print(_str + " is not a palindrome.")
---
+++ StringReader.py
@@ -0,0 +1,9 @@
+﻿import string
+
+# Read a string and interpret it as an array of machine values
+# WHAT IS THIS ASKING ??????
+_str = input("Enter the string to be interpreted: ")
+_list = list(_str)
+print(_list)
+_ba = bytearray(_str, 'ascii')
+print(_ba)
