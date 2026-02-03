a7baa5f5dcbe6e06850c52c9771e8784edeea897
dimbled <aaron.erlanger@gmail.com>
2026-02-03 08:54:17 -0500

    started PenguinSets.py and added Penguins_Data.csv

---
+++ PenguinSets.py
@@ -0,0 +1,7 @@
+﻿# IMPORTS
+import csv
+with open('C:/Users/aaron/PycharmProjects/CSDS237/Penguins_Data.csv') as f:
+    penguins =[{k: v for k, v in row.items()} for row in csv.DictReader(f)]
+
+# male_penguin_species = penguins["MALE"]
+# female_penguin_species = penguins["FEMALE"]
