0f968356cbdcaf5c723c415148d360113bec2110
Aaron <aaron.erlanger@gmail.com>
2026-02-03 18:10:30 -0500

    finished LowercaseUnderscoreFinder.py

--- LowercaseUnderscoreFinder.py
+++ LowercaseUnderscoreFinder.py
@@ -1,12 +1,7 @@
 ﻿#IMPORTS
-
+import re
 # Find sequences of lowercase letters joined by an underscore.
-# ASK FOR MORE SPECIFICITY
 
-str = 'WEATher_is_14deg_less_than_NORMAL65'
-str_list = []
-temp_str = ""
-
-for letter in str:
-
-    temp_str.join(filter(str.isalpha, _str))
+str = 'WEATher_is_14deg_less_than_NORMAL65' # initialize
+str_list = re.findall("[a-z_]+", str) # make list of series of lower alphas and underscores of any length
+print(str_list)
