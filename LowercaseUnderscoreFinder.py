e196c08ec4a295558fedf76a03d8c472cac611d0
Aaron <aaron.erlanger@gmail.com>
2026-02-03 20:47:12 -0500

    adjusted some minor formatting

--- LowercaseUnderscoreFinder.py
+++ LowercaseUnderscoreFinder.py
@@ -4,4 +4,4 @@ import re
 # Find sequences of lowercase letters joined by an underscore.
 _str = 'WEATher_is_14deg_less_than_NORMAL65' # initialize
 str_list = re.findall("[a-z_]+", _str) # make list of series of lower alphas and underscores of any length
-print(str_list)
+print(f"The sequences of lowercase letters joined by an underscore are: {str_list}.")
